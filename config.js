77ac9763bb364e1f4c3235f9e2eb9a46ac284ef0
Vartika1007 <64276391+Vartika1007@users.noreply.github.com>
2021-03-29 21:10:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBJsTqwL8yjTWW8mrCWgrBird7HI944SIw",
-  authDomain: "book-santa.firebaseapp.com",
-  databaseURL: "https://book-santa.firebaseio.com",
-  projectId: "book-santa",
-  storageBucket: "book-santa.appspot.com",
-  messagingSenderId: "546422247592",
-  appId: "1:546422247592:web:a82af183e05f978755f29d",
-  measurementId: "G-MLCQNSGDLH"
-};
+  apiKey: "AIzaSyDQn-fm5RLvmExS1VUamaf5_gE6o6pkqIA",
+        authDomain: "book-santa-11705.firebaseapp.com",
+        databaseURL: "https://book-santa-11705.firebaseio.com",
+        projectId: "book-santa-11705",
+        storageBucket: "book-santa-11705.appspot.com",
+        messagingSenderId: "570024860444",
+        appId: "1:570024860444:web:56fe074322d99210e078d9"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
